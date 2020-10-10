4d490e3414d9cf6aee253867a6677ce31ebdeae3
ivankorolenko <korolenko.i.u@gmail.com>
2020-10-10 18:07:29 +0300

    Updates

---
+++ precache-manifest.e2131e5cb6140a4b4e69244e767912eb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "856da4090b2a963b51166cdec476bf7b",
+    "url": "/GameOfTrones__React/index.html"
+  },
+  {
+    "revision": "cacb20635b6689eee56d",
+    "url": "/GameOfTrones__React/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "f23fb463393dd0566e7a",
+    "url": "/GameOfTrones__React/static/css/main.4fd1c523.chunk.css"
+  },
+  {
+    "revision": "cacb20635b6689eee56d",
+    "url": "/GameOfTrones__React/static/js/2.27f1a6da.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/GameOfTrones__React/static/js/2.27f1a6da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f23fb463393dd0566e7a",
+    "url": "/GameOfTrones__React/static/js/main.841ef36b.chunk.js"
+  },
+  {
+    "revision": "649f78adf8ef2c717645",
+    "url": "/GameOfTrones__React/static/js/runtime-main.b346b5f7.js"
+  },
+  {
+    "revision": "c9fa41e6fdf2a516024409f1f9004a6d",
+    "url": "/GameOfTrones__React/static/media/error.c9fa41e6.jpg"
+  },
+  {
+    "revision": "50db5a81ea3e8cb18057b63288fa34ec",
+    "url": "/GameOfTrones__React/static/media/got.50db5a81.jpeg"
+  }
+]);
